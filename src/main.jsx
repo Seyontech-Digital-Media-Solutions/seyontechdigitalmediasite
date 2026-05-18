c86b78a081f9b7a83677d2bbc3abecb1dcade0b1
Swetha <swethasenthilkmr2@gmail.com>
2026-05-18 13:19:15 +0530

    fix: add basename for GitHub Pages routing

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/seyontechdigitalmedia-main">
+  <BrowserRouter basename="/seyontechdigital">
     <App />
   </BrowserRouter>
 );
