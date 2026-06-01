0be174afa5f0a562b53121d8bead5e3a13c41336
Velmora Oils <velmoraoils@gmail.com>
2026-06-01 13:16:41 +0530

    fix router basename for seyontech.in

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,8 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
-const basename =
-  import.meta.env.MODE === "production"
-    ? "/seyontechdigitalmediasite"
-    : "/";
-
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename={basename}>
+  <BrowserRouter>
     <App />
   </BrowserRouter>
 );
