c2ac67755c4f1868134e3ae32cf7a79ad2aab806
swetha-kmr <swethasenthilkmr2@gmail.com>
2026-06-12 15:11:40 +0530

    Deploy updated build

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/seyontechdigitalmediasite/',
+  base: process.env.NODE_ENV === 'production'
+    ? '/seyontechdigitalmediasite/'
+    : '/',
   build: {
     outDir: 'docs'
   }
