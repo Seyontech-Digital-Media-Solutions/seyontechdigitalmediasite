e04986adc04fd4816aa1425b9bef3b563c13952b
swetha-kmr <swethasenthilkmr2@gmail.com>
2026-06-12 15:33:54 +0530

    Fix GitHub Pages routing

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ export default function App() {
 
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/work" element={<OurWork />} />
+<Route path="/work" element={<OurWork />} />
       </Routes>
     </>
   );
