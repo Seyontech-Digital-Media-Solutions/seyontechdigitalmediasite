34104bd4b562b4e3b936f8cab5391cee03aaa3fd
Swetha <swethasenthilkmr2@gmail.com>
2026-07-14 13:29:22 +0530

    Fix SEO: title, meta description, favicon, basename, add Google verification file

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/seyontechdigitalmediasite">
+  <BrowserRouter basename={import.meta.env.BASE_URL}>
     <App />
   </BrowserRouter>
 );
